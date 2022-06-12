723b8dbcb9a2640a1fff41207f765a50260ed653
Hieu Tran <hieu.tran@enouvo.com>
2022-06-12 17:59:28 +0700

    update code list to directory hotels url

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
@@ -10,6 +14,12 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
     </div>
   );
--- booking/src/pages/list/List.jsx
+++ booking/src/pages/list/List.jsx
@@ -1,11 +1,55 @@
-import Header from "../../components/header/Header";
+import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+            </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default List;
