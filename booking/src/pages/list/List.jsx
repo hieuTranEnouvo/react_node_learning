157f7a720573d0f4d00719ff138d78e4c8024bd3
Hieu Tran <hieu.tran@enouvo.com>
2022-10-01 23:32:42 +0700

    change image, add list

--- booking/src/pages/list/List.jsx
+++ booking/src/pages/list/List.jsx
@@ -1,8 +1,16 @@
 import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
 
 const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
   return (
     <div>
       <Navbar />
@@ -13,9 +21,14 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Destination</label>
+              <input placeholder={destination} type="text" />
             </div>
             <div className="lsItem">
               <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yyyy"
+              )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
             </div>
             <div className="lsItem">
               <label>Options</label>
